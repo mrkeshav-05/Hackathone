8fda77a5c65b92d16855945e9c58e051bee766f1
mrkeshav.05 <keshavpundhir69@gmail.com>
2024-10-12 16:51:43 +0530

    artist sigup model completed

---
+++ src/models/artist.model.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { generateAccessToken } from '../utils/tokens/generateAccessToken.js';
+import { generateRefreshToken } from '../utils/tokens/generateRefreshToke.js';
+import { generateEmailVerificationToken } from '../utils/tokens/generateEmailVerificationToken.js';
+
+const artistSchema = new mongoose.Schema({
+  artistName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profilePicture: {
+    type: String,
+  },
+  bio: {
+    type: String,
+  },
+  genres: [
+    {
+      type: String
+    }
+  ]
+}, { timestamps: true });
+
+
+artistSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+})
+
+artistSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+  // password is the password entered by the user
+  // this.password is the hashed password in the database
+}
+
+artistSchema.methods.generateAccessToken = async function () {
+  return generateAccessToken(this);
+}
+
+artistSchema.methods.generateRefreshToken = async function () {
+  return generateRefreshToken(this);
+}
+
+artistSchema.methods.generateEmailVarificationToken = async function () {
+  return generateEmailVerificationToken(this);
+}
+
+
+export const Customer = mongoose.model('Customer', artistSchema);
